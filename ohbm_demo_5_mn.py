0e225ff3b6d6b89fd61b193ca2b950462b9f7762
Jerome Dockes <jerome@dockes.org>
2019-06-12 08:35:10 +0200

    iter

--- ohbm_demo_5_mn.py
+++ ohbm_demo_5_mn.py
@@ -18,9 +18,14 @@ warnings.simplefilter('ignore')
 # ------------------------------------------------------------------
 
 # Download and plot a group-level statistical map:
-from nilearn import datasets, plotting
+from nilearn import datasets
 
 img = datasets.fetch_neurovault_motor_task()['images'][0]
+print(img)
+
+######################################################################
+from nilearn import plotting
+
 plotting.view_img(img, threshold='95%')
 
 ######################################################################
@@ -39,6 +44,10 @@ plotting.plot_stat_map(
 plotting.view_img_on_surf(img, threshold='95%')
 
 ######################################################################
+
+plotting.view_img_on_surf(img, threshold='95%', surf_mesh='fsaverage')
+
+######################################################################
 # More about dataset downloaders: https://nilearn.github.io/modules/reference.html#module-nilearn.datasets
 #
 # More about plotting: https://nilearn.github.io/plotting/index.html
@@ -50,12 +59,13 @@ plotting.view_img_on_surf(img, threshold='95%')
 # we use one subject from a dataset recently added to Nilearn:
 # a movie watching based brain development dataset
 
-rest_data = datasets.fetch_development_fmri(n_subjects=1)
+rest_data = datasets.fetch_development_fmri(n_subjects=60)
 rest_data.keys()
 
 ###############################################################################
 # Extract time series from probabilistic ROIs of the MSDL atlas.
 from nilearn.input_data import NiftiMapsMasker
+import numpy as np
 
 msdl = datasets.fetch_atlas_msdl()
 print('number of regions in MSDL atlas:', len(msdl.labels))
@@ -63,35 +73,46 @@ print('number of regions in MSDL atlas:', len(msdl.labels))
 masker = NiftiMapsMasker(
     msdl.maps, resampling_target="data", t_r=2, detrend=True,
     low_pass=.1, high_pass=.01, memory='nilearn_cache').fit([])
-masked_data = masker.transform(rest_data.func[0], rest_data.confounds[0])
-print('masked data shape:', masked_data.shape)
+masked_data = [masker.transform(func, confounds) for
+               (func, confounds) in zip(rest_data.func, rest_data.confounds)]
+masked_data = np.asarray(masked_data)
+print('masked data shape:', masked_data[0].shape)
 
 ###############################################################################
 # Compute and plot connectivity matrix
 
 from nilearn.connectome import ConnectivityMeasure
 
-correlation = ConnectivityMeasure(
-    kind='correlation').fit_transform([masked_data])[0]
+correlation_measure = ConnectivityMeasure(kind='correlation').fit(masked_data)
 
-plotting.plot_matrix(correlation, tri='lower')
+plotting.plot_matrix(correlation_measure.mean_, tri='lower')
 
 ###############################################################################
 plotting.view_connectome(
-    correlation, msdl.region_coords, threshold='90%', cmap='cold_hot')
+    correlation_measure.mean_, msdl.region_coords,
+    threshold='90%', cmap='cold_hot')
 
 ###############################################################################
-# `ConnectivityMeasure` can be used to extract features for supervised learning
+# Age group classification with scikit-learn
+# ------------------------------------------
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score
 
-connectivity = ConnectivityMeasure(vectorize=True).fit_transform(
-    [masked_data, masked_data, masked_data])
-print('(n samples, n features):', connectivity.shape)
+kinds = ['correlation', 'partial correlation', 'tangent']
+groups = [pheno['Child_Adult'] for pheno in rest_data.phenotypic]
+classes = LabelEncoder().fit_transform(groups)
+
+cv = StratifiedShuffleSplit(n_splits=15, random_state=0, test_size=10)
+
+correlations = ConnectivityMeasure(
+    kind='correlation', vectorize=True).fit_transform(masked_data)
+
+scores = cross_val_score(LinearSVC(), correlations, classes, cv=cv)
+print(np.mean(scores))
 
 ###############################################################################
-# More about connectivity with nilearn: https://nilearn.github.io/connectivity/index.html
-#
-# More about the development dataset:
-# Richardson et al. (2018). Development of the social brain from age three to
-# twelve years.
-#
-# Using this dataset and connectivity to classify adults vs children: https://github.com/nilearn/nilearn/blob/master/examples/03_connectivity/plot_group_level_connectivity.py
+import seaborn as sns
+sns.violinplot(scores)
+sns.stripplot(scores, color='k')
